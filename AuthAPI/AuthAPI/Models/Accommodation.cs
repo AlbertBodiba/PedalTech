5072ba2c73b2b13e6cd453b71e514447d477fba4
AlbertBodiba <u21462594@up.ac.za>
2024-07-31 11:02:45 +0200

    ModelPush

--- AuthAPI/AuthAPI/Models/Accommodation.cs
+++ AuthAPI/AuthAPI/Models/Accommodation.cs
@@ -3,15 +3,14 @@
     public class Accommodation
     {
         public int EventAccommodationId { get; set; }
-
-        public int? BikeDropEventId { get; set; }
-
         public string? AccommodationName { get; set; }
 
         public string? AccommodationDescription { get; set; }
 
-        public virtual ICollection<AccommodationType> AccommodationTypes { get; set; } = new List<AccommodationType>();
+        //public int? BikeDropEventId { get; set; }
 
-        public virtual BikeDropEvent? BikeDropEvent { get; set; }
+        //public virtual ICollection<AccommodationType> AccommodationTypes { get; set; } = new List<AccommodationType>();
+
+        //public virtual BikeDropEvent? BikeDropEvent { get; set; }
     }
 }
