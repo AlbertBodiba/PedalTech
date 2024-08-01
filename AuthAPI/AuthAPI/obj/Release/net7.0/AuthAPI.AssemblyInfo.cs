53edffb9e7e22672ec414549aeb6461b2032a714
AlbertBodiba <u21462594@up.ac.za>
2024-08-01 12:42:26 +0200

    Ss

--- AuthAPI/AuthAPI/obj/Release/net7.0/AuthAPI.AssemblyInfo.cs
+++ AuthAPI/AuthAPI/obj/Release/net7.0/AuthAPI.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthAPI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68e26603aada3d38675dd0fafbff9b0734f5bf91")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthAPI")]
